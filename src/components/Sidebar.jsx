348e8c59794f51dff1f14324228fd5034c02d41b
Muthoni Kevin Gatamu <49341231+KevGMR@users.noreply.github.com>
2022-06-28 22:28:26 +0300

    ecomerce component update

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -8,7 +8,13 @@ import { useStateContext } from '../contexts/ContextProvider';
 import { links } from '../data/dummy';
 
 const Sidebar = () => {
-    const { activeMenu, setActiveMenu } = useStateContext()
+    const { activeMenu, setActiveMenu, screenSize } = useStateContext()
+
+    const handleCloseSidebar = () => {
+        if (activeMenu && screenSize <= 900) {
+            setActiveMenu(false)
+        }
+    }
     
     const activeLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-white text-md m-2'
     const normalLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-white text-md text-gray-700 dark:text-gray-200 dark:hover:text-black hover:bg-light-gray m-2'
@@ -17,7 +23,7 @@ const Sidebar = () => {
             {activeMenu && (
                 <>
                     <div className="flex justify-between items-center">
-                        <Link to='/' onClick={() => setActiveMenu(false)} className='items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900' ><SiShopware /> <span>Dash-Kev</span> </Link>
+                        <Link to='/' onClick={handleCloseSidebar} className='items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900' ><SiShopware /> <span>Dash-Kev</span> </Link>
                         <TooltipComponent content='Menu' position='BottomCenter' >
                             <button type='button' onClick={() => { setActiveMenu((prevActiveMenu) => !prevActiveMenu) }} className='text-xl rounded-full p-3 hover:bg-light-gray mt-4 block md:hidden' >
                                 <MdOutlineCancel />
